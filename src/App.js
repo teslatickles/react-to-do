89d8cafeb194f32905814896c10f6791d664edd7
teslatickles <hunterhartline@gmail.com>
2018-08-14 23:45:36 -0500

    modify ToDo.js and App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,24 @@ import './App.css';
 import ToDo from './components/ToDo.js';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [
+        { description: 'Play fetch with the cat', isCompleted: true },
+        { description: '3D Print a Darth Buddha', isCompleted: true },
+        { description: 'Convert my node/js app to React gui/web-frontend', isCompleted: false }
+      ]
+    };
+  }
+
   render() {
     return (
       <div className="App">
         <ul>
-          <ToDo />
-          <ToDo />
+          { this.state.todos.map( (todo, index) => 
+            <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } />
+          )}
         </ul>
       </div>
     );
